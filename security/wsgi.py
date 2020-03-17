f026ea5bc6c2f1c205fca3f4ab6bf18d763a8160
Tagone565 <62262872+Tagone565@users.noreply.github.com>
2020-03-17 22:16:19 +0000

    Update wsgi.py

--- security/wsgi.py
+++ security/wsgi.py
@@ -7,14 +7,9 @@ For more information on this file, see
 https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
 """
 
-import osfrom whitenoise import WhiteNoise
-
-from my_project import MyWSGIApp
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'security.settings')
 
 application = get_wsgi_application()
-application = MyWSGIApp()
-application = WhiteNoise(application, root='/path/to/static/files')
-application.add_files('/path/to/more/static/files', prefix='more-files/')
+
