5e8c6b2b916df92c0c53eab34d72181ac01825c5
Tagone565 <62262872+Tagone565@users.noreply.github.com>
2020-03-17 01:51:53 +0000

    Update settings.py

--- security/settings.py
+++ security/settings.py
@@ -14,7 +14,7 @@ SECRET_KEY = 'ciod2u+ue4m8$dbrsb6ou2-qz79id%tb-@dj1zg8c+_qzpue1!'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['afternoon-brushlands-76940.herokuapp.com','127.0.0.1']
+ALLOWED_HOSTS = ['afternoon-brushlands-76940.herokuapp.com']
 
 
 # Application definition
