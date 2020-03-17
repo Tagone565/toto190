81f544e17cd2ae426ba70486ece83ca97894c940
Tagone565 <62262872+Tagone565@users.noreply.github.com>
2020-03-17 23:58:56 +0000

    Update urls.py

--- security/urls.py
+++ security/urls.py
@@ -1,7 +1,7 @@
 
 from django.contrib import admin
 from django.conf.urls import url, include
-from .views import registerpage, home, Userlogout, service, contact, Aboutus, Myacount
+from .views import registerpage, loginpage, Userlogout, service, contact, Aboutus, Myacount
 from django.conf import settings
 from django.conf.urls.static import static
 
